0b397a688cb4f4b487b07d662706686ef08e32ed
David <shanpoch@chapalax.am>
2026-07-06 15:40:39 +0400

    routing and service

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { PackageListComponent } from './components/package-list/package-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: PackageListComponent }
+];
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,10 +1,22 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Package } from '../models/interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  constructor() { }
+
+  private http = inject(HttpClient);
   private baseUrl = 'mock-api';
 
-  constructor() { }
+  private depCache = new Map<string, string[]>();
+
+  getPackeges(): Observable<Package[]> {
+    return this.http.get<Package[]>(`${this.baseUrl}/packages`)
+  }
+
+  
 }
