dc29abbd9c37a0601bd3db4d89e434af9a9b8fab
David <shanpoch@chapalax.am>
2026-07-06 16:56:17 +0400

    toolbar and search

--- src/app/components/package-list/package-list.component.ts
+++ src/app/components/package-list/package-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { ApiService } from '../../services/api.service';
 import { Package } from '../../models/interface';
 
@@ -15,8 +15,22 @@ export class PackageListComponent {
   }
   private packageService = inject(ApiService);
   packages = signal<Package[]>([]);
+  searchString = signal<string>('');
 
+  filteredPackages = computed(() => {
+    const query = this.searchString().toLowerCase();
+    return this.packages().filter(pkg => 
+      pkg.id.toLowerCase().includes(query)
+    );
+  });
+  
+  onSearch(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    this.searchString.set(inputElement.value);
+    console.log("sdsd")
+  }
   loadPackages() {
+    
     this.packageService.getPackages().subscribe(res => {
       this.packages.set(res);
       console.log(res)
